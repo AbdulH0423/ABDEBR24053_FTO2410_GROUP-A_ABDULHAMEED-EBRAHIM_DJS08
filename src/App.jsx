e42e92441eb13184e169e6ac92c431fd221b1059
Abdul-Hameed Zainud-Deen Ebrahim <zain.ebrahim8@gmail.com>
2025-04-06 15:30:04 +0200

    Updated ap.jsx with imports & routing components

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,63 @@
-/**
- * Challenge:
- * Bootstrap the VanLife project by creating the first 2 routes:
- * Home and About.
- * 
- * Also include the navbar that can link between the two routes.
- * For now, you'll either need to copy/paste the navbar code
- * to both Home and About pages, or you'll need to find a place
- * to put it where it can be shared between the two pages.
- * (Don't overthink this part - just do whatever is easiest for
- * you because we'll learn a better approach very soon)
- * 
- * Review challenge: do all the CSS yourself based on the design
- * linked in the slides.
- */
+import { BrowserRouter, Routes, Route, } from "react-router-dom"
+import Home from "./Pages/Home"
+import About from "./Pages/About"
+import Vans from "./Pages/Vans/Vans"
+import VanDetail from "./Pages/Vans/VanDetail"
+import Login from "./Pages/Login"
+import Dashboard from "./Pages/Host/Dashboard"
+import Income from "./Pages/Host/Income"
+import Reviews from "./Pages/Host/Reviews"
+import HostVans from "./Pages/Host/HostVans"
+import HostVanDetail from "./Pages/Host/HostVanDetail"
+import HostVanInfo from "./Pages/Host/HostVanInfo"
+import HostVanPricing from "./Pages/Host/HostVanPricing"
+import HostVanPhotos from "./Pages/Host/HostVanPhotos"
+import NotFound from "./Pages/NotFound"
+import Layout from "./Components/Layout"
+import HostLayout from "./Components/HostLayout"
+import AuthRequired from "./Components/AuthRequired"
 
 function App() {
+  /**
+   * Challenge: Create the AuthRequired Layout Route to protect
+   * all the /host routes.
+   * 
+   * For now, just use `const authenticated = false`
+   * to determine the authenticated status of the user, and
+   * either send them to the /login route, or render the Outlet
+   */
 
   return (
-    <h1>Start here</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          <Route
+            path="login"
+            element={<Login />}
+          />
+
+          <Route element={<AuthRequired />}>
+            <Route path="host" element={<HostLayout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="income" element={<Income />} />
+              <Route path="reviews" element={<Reviews />} />
+              <Route path="vans" element={<HostVans />} />
+              <Route path="vans/:id" element={<HostVanDetail />}>
+                <Route index element={<HostVanInfo />} />
+                <Route path="pricing" element={<HostVanPricing />} />
+                <Route path="photos" element={<HostVanPhotos />} />
+              </Route>
+            </Route>
+          </Route>
+
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
